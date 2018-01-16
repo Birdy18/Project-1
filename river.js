d2e56d1edf77d7eaef45ce01cb3fa9870f5cd867
Birdy18 <birdsallt@student.ncmich.edu>
2018-01-15 20:07:03 -0500

    I added console.clear to clean up the run0

--- river.js
+++ river.js
@@ -1,23 +1,28 @@
-"use strict";
+
+const BASE = 50000;
+const BEDCOST = 17000;
+const BATHCOST = 12500;
+const CARCOST = 6000;
+
 
 function main() {
+    console.clear();
     var nameComp = "River Falls Homes Construction Company";
     console.log(nameComp);
     var rls = require('readline-sync');
     var lotnum = rls.question("What is your lot number? ");
-    console.log("Lot Number = " + lotnum);
+    console.clear();
     var bednum = rls.question("How many bedrooms? ");
-    console.log("Number of bedrooms = " + bednum);
+    console.clear();
     var bathnum = rls.question("How many bathroooms? ");
-    console.log("Number of bathrooms = " + bathnum);
+    console.clear();
     var carnum = rls.question("How many cars in the garage? ");
-    console.log("Number of cars = " + carnum);
-    var base = 50000.00;
-    var bedcost = 17000.00 * bednum;
-    var bathcost = 12500.00 * bathnum;
-    var carcost = 6000.00 * carnum;
-    var Totcost = bedcost + bathcost + carcost + base;
-    console.log("TotalCost = " + Totcost);
+    console.clear();
+    var total_bed_cost = BEDCOST * bednum;
+    var total_bath_cost = BATHCOST * bathnum;
+    var total_car_cost = CARCOST * carnum;
+    var total_cost = BASE + total_bed_cost + total_bath_cost + total_car_cost;
+    console.log("TotalCost = " + total_cost);
 }
 
 main();
